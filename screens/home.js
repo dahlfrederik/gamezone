a3348ba5d6ac59332f3af9b798a6e453e2f2dce2
Frederik Dahl <cph-fd76@cphbusiness.dk>
2021-08-05 09:48:32 +0200

    New login method

--- screens/home.js
+++ screens/home.js
@@ -1,11 +1,16 @@
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, Button } from "react-native";
 import { globalStyles } from "../styles/global";
 
-export default function Home() {
+export default function Home({ navigation }) {
+  const pressHandler = () => {
+    navigation.navigate("ReviewDetails");
+  };
+
   return (
     <View style={globalStyles.container}>
       <Text style={globalStyles.titleText}>Home screen</Text>
+      <Button title="Go to review dets" onPress={pressHandler} />
     </View>
   );
 }
