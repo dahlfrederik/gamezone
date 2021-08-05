442f2b76781f66e568d78199118f2283b0466885
Frederik Dahl <cph-fd76@cphbusiness.dk>
2021-08-05 10:27:13 +0200

    React native tutorial #23 done

--- routes/appNavigator.js
+++ routes/appNavigator.js
@@ -3,18 +3,32 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import Home from "../screens/home";
 import ReviewDetails from "../screens/reviewDetails";
+import { BorderlessButton } from "react-native-gesture-handler";
 
 const Stack = createStackNavigator();
 
 export default Navigator = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={Home} options={{ title: "" }} />
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "#eee",
+            height: 90,
+          },
+          headerTintColor: "#444",
+        }}
+      >
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ title: "GameZone" }}
+        />
         <Stack.Screen
           name="ReviewDetails"
           component={ReviewDetails}
-          options={{ title: "" }}
+          options={{ title: "Review Details" }}
         />
       </Stack.Navigator>
     </NavigationContainer>
