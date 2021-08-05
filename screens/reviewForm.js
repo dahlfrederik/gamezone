69177ffefcc7c301700587143cfcaf58fc10f1cb
Frederik Dahl <cph-fd76@cphbusiness.dk>
2021-08-05 14:15:16 +0200

    React native tutorial #30 done

---
+++ screens/reviewForm.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import { StyleSheet, View, Text, TextInput, Button } from "react-native";
+import { globalStyles } from "../styles/global";
+import { Formik } from "formik";
+
+export default function ReviewForm() {
+  return (
+    <View style={globalStyles.container}>
+      <Formik
+        initialValues={{ Title: "", Body: "", Rating: "" }}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+      >
+        {(props) => (
+          <View>
+            <TextInput
+              style={globalStyles.input}
+              placeholder="Review title"
+              onChangeText={props.handleChange("title")}
+              value={props.values.title}
+            />
+            <TextInput
+              multiline
+              style={globalStyles.input}
+              placeholder="Review body"
+              onChangeText={props.handleChange("body")}
+              value={props.values.body}
+            />
+            <TextInput
+              style={globalStyles.input}
+              placeholder="Rating (1-5)"
+              onChangeText={props.handleChange("rating")}
+              value={props.values.rating}
+              keyboardType="numeric"
+            />
+            <Button
+              title="submit"
+              color="maroon"
+              onPress={props.handleSubmit}
+            />
+          </View>
+        )}
+      </Formik>
+    </View>
+  );
+}
